556e0edd2725b333233ea855cea575a57a95ad8d
Nicolás Wagner <nicolas.wagner@correo.ucu.edu.uy>
2026-06-15 11:27:43 -0300

    Seeded host countries and added the country assigned to admin table

---
+++ src/TicketingMundialUCU/Services/HostCountries.cs
@@ -0,0 +1,15 @@
+namespace TicketingMundialUCU.Services;
+
+public static class HostCountries
+{
+    public const string Canada = "Canadá";
+    public const string Mexico = "México";
+    public const string UnitedStates = "Estados Unidos";
+
+    public static readonly string[] All =
+    [
+        Canada,
+        Mexico,
+        UnitedStates
+    ];
+}
